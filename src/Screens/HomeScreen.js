bbe96e704ea14f5c763d22106d6cf71c5132c3d2
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-20 09:59:05 +0200

    front-end

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { Card } from "../Components/Cards/Card";
 import { VideoCard } from "../Components/Cards/VideoCard";
 import { Jumbotron } from "../Components/Jumbotron";
 import { Nav } from "../Components/Nav/Nav";
-import { APP_ICONS, DISCOVER } from "../context/settings";
+import { APP_ICONS, DISCOVER, TRENDING } from "../context/settings";
 import illustration from "../Images/anshita-nair-0rxLLHD1XxA-unsplash.jpg";
 
 export const HomeScreen = () => {
@@ -25,11 +25,21 @@ export const HomeScreen = () => {
         }
       />
       <div className="section_one">
-        <h2 className="card_header">{"Discover"}</h2>
-        <div className="section_one_grid">
-          {DISCOVER.map((e, i) => {
-            return <Card key={i} item={e} header={"Discover"} />;
-          })}
+        <div className="section_one_information">
+          <h2 className="card_header">{"Discover"}</h2>
+          <div className="section_one_grid">
+            {DISCOVER.map((e, i) => {
+              return <Card key={i} item={e} header={"Discover"} />;
+            })}
+          </div>
+        </div>
+        <div className="section_one_information">
+          <h2 className="card_header">{"Trending"}</h2>
+          <div className="section_one_grid">
+            {TRENDING.map((e, i) => {
+              return <Card key={i} item={e} header={"Discover"} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/context/settings.js
+++ src/context/settings.js
@@ -61,4 +61,49 @@ export const DISCOVER = [
     img: "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1607362705516-R5Q22H4FVIVPNMW8OWD7/SamSpratt_KidCudi_ManOnTheMoon3_AlbumCover_Web.jpg",
     time_length: "5:21",
   },
+  {
+    id: 5,
+    title: "The Chainsmokers - Don't Let Me Down",
+    views: "723,221",
+    img: "https://images.nightcafe.studio/jobs/nrOcbGDvjDgIZPO7rbA6/nrOcbGDvjDgIZPO7rbA6.jpg",
+    time_length: "2:00",
+  },
+];
+
+export const TRENDING = [
+  {
+    id: 1,
+    title: "Shawn Mednes - Treats You Better",
+    views: "1,854,691,298",
+    img: "https://routenote.com/blog/wp-content/uploads/2022/08/greatest-album-covers-of-all-time.png",
+    time_length: "2:45",
+  },
+  {
+    id: 2,
+    title: "Ed Sheeran - Shape of You",
+    views: "681,298",
+    img: "https://www.fubiz.net/wp-content/uploads/2018/01/01-kendrick-lamar-damn-album-art-2017-billboard-1240.jpg",
+    time_length: "4:21",
+  },
+  {
+    id: 3,
+    title: "Marshmello & Anne-Marie Friends",
+    views: "1,584,243,222",
+    img: "https://cdn.musebycl.io/2021-03/travisscott_astroworld_hed_2021.jpg",
+    time_length: "2:30",
+  },
+  {
+    id: 4,
+    title: "The Chainssmokers & Coldplay - Something",
+    views: "728,883",
+    img: "https://blog.dozmia.com/content/images/2019/01/Portrait-The-Weeknd.jpg",
+    time_length: "5:21",
+  },
+  {
+    id: 5,
+    title: "The Chainsmokers - Don't Let Me Down",
+    views: "723,221",
+    img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Guide-to-Album-Covers.jpg",
+    time_length: "2:00",
+  },
 ];
